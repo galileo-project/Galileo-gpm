4d34abd0f2d69287361fa67252755a81ae8db8c1
ColeChan <chenwenjie27@gmail.com>
2016-04-11 12:01:19 +0800

    Init project

---
+++ gpm/src/utils/conf.py
@@ -0,0 +1,46 @@
+import yaml
+
+class ConfReader:
+    def __init__(self, path):
+        self.path = path
+        self.content = {}
+        
+    def read(self):
+        with open(self.path, "r") as stream:
+            self.content = yaml.load(stream)
+        
+    @property
+    def author(self):
+        return self.content.get("author")
+
+    @property
+    def description(self):
+        return self.content.get("description")
+
+    @property
+    def version(self):
+        return self.content.get("version")
+
+    @property
+    def email(self):
+        return self.content.get("email")
+
+    @property
+    def language(self):
+        return self.content.get("language")
+
+    @property
+    def build(self):
+        return self.content.get("build")
+
+    @property
+    def install(self):
+        return self.content.get("install")
+
+    @property
+    def test(self):
+        return self.content.get("test")
+
+    @property
+    def dep(self):
+        return self.content.get("dep")
