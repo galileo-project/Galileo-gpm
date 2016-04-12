02fb3f0f1056f3275d8e3f78e4582eb35b12cb31
ColeChan <chenwenjie27@gmail.com>
2016-04-12 16:18:17 +0800

    Update cli

--- gpm/cli/__init__.py
+++ gpm/cli/__init__.py
@@ -1,14 +1,15 @@
 import pkgutil
 from gpm.utils.opt import opt_parser
 from gpm.utils.log import Log
-from gpm import config
+from gpm.config import version
 from gpm.utils.console import put
 
 class CLI:
+    _OPTS = {"shortcut": "v", "fullname": ["version"], "action": ["_version"]}
+
     def __init__(self, args):
-        self._args  = args
-        self._mods  = {}
-        self.define = {"shortcut": "v", "fullname": ["version"], "action": [self.version]}
+        self._args = args
+        self._mods = {}
 
     @property
     def mods(self):
@@ -25,8 +26,8 @@ class CLI:
         if self._args in self.mods.keys():
             self.mods[self._args[0]](self._args[1:]).run()
         else:
-            func, args = opt_parser(self._args, self, self.define)
-            func(*args)
+            func, kwargs = opt_parser(self._args, self)
+            func(**kwargs)
 
-    def version(self, *args, **kwargs):
-        put(config.version)
+    def _version(self, *args, **kwargs):
+        put(version)
--- gpm/cli/build.py
+++ gpm/cli/build.py
@@ -1,6 +1,13 @@
 class CLIBuild:
+    OPTS = {"shortcut": "", "fullname": [], "action": []}
+
     def __init__(self, args):
         self.args = args
 
     def run(self):
         pass
+
+    def _build(self):
+        pass
+
+_NAME = CLIBuild
