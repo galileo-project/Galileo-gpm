c8e001914f0a5e67e6638a9cca880564b6db9eac
ColeChan <chenwenjie27@gmail.com>
2016-04-12 16:18:42 +0800

    Update opt parse func

--- gpm/utils/log.py
+++ gpm/utils/log.py
@@ -1,5 +1,5 @@
-from utils.console import put
-from utils.color import yellow, red
+from gpm.utils.console import put
+from gpm.utils.color import yellow, red, magenta
 import sys
 
 class Log:
@@ -11,3 +11,7 @@ class Log:
     def fatal(cls, msg):
         put(red("FATAL: %s" % msg))
         sys.exit(1)
+
+    @classmethod
+    def debug(cls, msg):
+        put(magenta("DEBUG: %s" % msg))
