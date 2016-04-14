7f1b702ffa777d7c7d46725007921ca7fdb5e796
ColeChan <chenwenjie27@gmail.com>
2016-04-14 11:59:57 +0800

    Update setup.py del long description

--- setup.py
+++ setup.py
@@ -7,18 +7,12 @@ URL          = "https://github.com/tor4z/Galileo-gpm"
 LICENSE      = "MIT License"
 VERSION      = __import__(PACKAGE).__version__
 
-import os
-import codecs
 from setuptools import setup, find_packages
-
-def read(fname):
-    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=read("README.md"),
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license=LICENSE,
