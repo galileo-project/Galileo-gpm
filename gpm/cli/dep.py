1cd2f699f16ce660ecdf3b4272918b545f379e4d
ColeChan <chenwenjie27@gmail.com>
2016-04-14 18:04:16 +0800

    Update sub cmd template

---
+++ gpm/cli/dep.py
@@ -0,0 +1,15 @@
+from gpm.utils.console import puts
+from gpm.cli import CLI
+from gpm.utils.operation import LocalOperation
+
+class CLIDep(CLI):
+    _OPTS    = {"shortcut": "h", "fullname": ["help"], "action": ["_help"], "default": "_help"}
+    __doc__ = """
+
+    """
+
+    def _help(self, *args, **kwargs):
+        puts(self.__doc__)
+
+
+_MOD = CLIDep
