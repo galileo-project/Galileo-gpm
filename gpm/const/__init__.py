3b99accf4b188f2af996faf7d1d81083dcaee2d6
ColeChan <chenwenjie27@gmail.com>
2016-04-15 10:39:22 +0800

    Update gpm const conf

--- gpm/const/__init__.py
+++ gpm/const/__init__.py
@@ -1,3 +1,4 @@
 DEFAULT_MOD = "default"
 GPM_YML     = ".gpm.yml"
 SYS_CONF    = "~/.gpm/gpm.conf"
+GPM_VAR     = "/var/cache/gpm"
--- gpm/const/status.py
+++ gpm/const/status.py
@@ -3,7 +3,7 @@ from gpm.const import GPM_YML
 Status = {"STAT_OPT_INVALID":           "Option invalid",
           "STAT_LOGIN_GITHUB_FAILED":   "Login GitHub failed",
           "STAT_GPM_CONF_EXIST":        "%s already exist" % GPM_YML,
-          "STAT_SYS_CONF_EXIST":        "%s already exist" % GPM_YML,
+          "STAT_SYS_CONF_EXIST":        "gpm configure already exist",
           "STAT_EXEC_ERROR":            "Execute command error",
           "STAT_BUILD_ERROR":           "Build error: %s",
           "STAT_LOAD_MOD_ERROR":        "Load %s error.",
