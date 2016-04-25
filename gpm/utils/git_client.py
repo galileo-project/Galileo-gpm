1faebd6106d9c8b704390c6444236d408dcc57aa
ColeChan <chenwenjie27@gmail.com>
2016-04-25 15:41:42 +0800

    Fix get github url

--- gpm/utils/git_client.py
+++ gpm/utils/git_client.py
@@ -21,8 +21,7 @@ class GitClient(LocalOperation):
     @property
     def github_url(self):
         if not self.__github_url:
-            user_url = self._config.git_url or "git@github.com:%s" % self.user_account[0]
-            self.__github_url = self.safe_urljoin(user_url, "%s.git" % self._config.name)
+            self.__github_url = self._config.git_url or "git@github.com:%s/%s" % (self.user_account[0], self._config.name)
         return self.__github_url
 
     @property
